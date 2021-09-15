cad87c4a952c957d37ff0665d858fd027b87c896
gernot-ohner <gernot.ohner@gmail.com>
2021-09-15 13:16:15 -0600

    Add test

--- tests/check_vm.c
+++ tests/check_vm.c
@@ -8,20 +8,55 @@
 #define TOLERANCE 0.00001
 
 
+Chunk c;
+
 void setup(void)
 {
     initVM();
+    initChunk(&c);
 }
 
 void teardown(void)
 {
+    freeChunk(&c);
 }
 
-START_TEST(test_money_create)
+START_TEST(test_subtraction_and_multiplication) {
+
+    { // set up chunk
+        int constant = addConstant(&c, 1.2);
+        writeChunk(&c, OP_CONSTANT, 123);
+        writeChunk(&c, constant, 123);
+
+        constant = addConstant(&c, 3.4);
+        writeChunk(&c, OP_CONSTANT, 123);
+        writeChunk(&c, constant, 125);
+
+        writeChunk(&c, OP_SUBTRACT, 126);
+
+        constant = addConstant(&c, 5.6);
+        writeChunk(&c, OP_CONSTANT, 123);
+        writeChunk(&c, constant, 127);
+
+        writeChunk(&c, OP_MULTIPLY, 128);
+        writeChunk(&c, OP_RETURN, 123);
+    } // set up chunk
+
+    VM* vm = getVm();
+    const Value expected = -12.32;
+
+    // when
+    interpretChunk(&c);
+
+    // then
+    Value actual = *(vm->stackTop);
+    ck_assert_double_eq_tol(actual, expected, TOLERANCE);
+}
+END_TEST
+
+START_TEST(test_additon_and_division)
 {
 
-    Chunk c;
-    initChunk(&c);
     { // set up chunk
         int constant = addConstant(&c, 1.2);
         writeChunk(&c, OP_CONSTANT, 123);
@@ -51,7 +86,6 @@ START_TEST(test_money_create)
     ck_assert_double_eq_tol(actual, expected, TOLERANCE);
 
     // finally
-    freeChunk(&c);
 }
 END_TEST
 
@@ -67,7 +101,8 @@ Suite * vm_suite(void)
     tc_core = tcase_create("Core");
 
     tcase_add_checked_fixture(tc_core, setup, teardown);
-    tcase_add_test(tc_core, test_money_create);
+    tcase_add_test(tc_core, test_additon_and_division);
+    tcase_add_test(tc_core, test_subtraction_and_multiplication);
     suite_add_tcase(s, tc_core);
 
     // I left this here for a template of how to add additional test cases
